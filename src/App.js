f8b1f4d3f5a7670e38f83aafaba6ca6b1361bc92
ddessilvestri <silvestri.des@gmail.com>
2022-10-21 18:52:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <header className="header">
+      <nav className="nav">
+      
+        <ul className="nav-menu">
+          <li className="nav-menu-item">
+            <a href="#" className="logo nav-link">
+              <img src= './logo512.png' alt='logo' className='logo' />    
+            </a>
+          </li>
+            <li className="nav-menu-item company-name">
+              <a href="#" className="nav-menu-link nav-link ">QB</a>
+          </li>
+        </ul>
+
+        <ul className="nav-menu">
+          <li className="nav-menu-item">
+            <a href="#" className="nav-menu-link nav-link">Blog</a>
+          </li>
+          <li className="nav-menu-item">
+            <a href="#" className="nav-menu-link nav-link">Videos</a>
+          </li>
+          <li className="nav-menu-item">
+            <a href="#" className="nav-menu-link nav-link">Sobre mí</a>
+          </li>
+          <li className="nav-menu-item">
+            <a href="#" className="nav-menu-link nav-link nav-menu-link_active"
+              >Contacto</a
+            >
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </>
+
+    // <div classNameName="App">
+    //   <Navbar/>
+    //   <header classNameName="App-header">
+    //     <img src={logo} classNameName="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       classNameName="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/Items.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Items = ({name}) => {
+  return (
+    
+    <div className='navbar-item'>{name}</div>
+  )
+}
