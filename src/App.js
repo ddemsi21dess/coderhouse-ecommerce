02eff2feaa9477d9328e9da3bd4f0116865ebea5
ddessilvestri <silvestri.des@gmail.com>
2022-10-21 19:23:49 -0300

    create NavbarItem component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,42 @@
 import './App.css';
-import { Navbar } from './components/Navbar';
+import { NavbarItem } from './components/NavbarItem';
+
+const menuItems = [
+  {name:'TVs'       ,id:'cat-1', isActive:false},
+  {name:'Smarphones',id:'cat-2', isActive:false},
+  {name:'Speakers'  ,id:'cat-3', isActive:true},
+  {name:'Headphones',id:'cat-4', isActive:false},
+  {name:'SmartWatch',id:'cat-5', isActive:false},
+]
 
 function App() {
   return (
     <>
-   <header className="header">
-      <nav className="nav">
-      
-        <ul className="nav-menu">
-          <li className="nav-menu-item">
-            <a href="#" className="logo nav-link">
-              <img src= './logo512.png' alt='logo' className='logo' />    
-            </a>
-          </li>
-            <li className="nav-menu-item company-name">
-              <a href="#" className="nav-menu-link nav-link ">QB</a>
-          </li>
-        </ul>
+    
+      <header className="header">
+          <nav className="nav">
+          
+            <ul className="nav-menu">
+              <li className="nav-menu-item">
+                <a href="#" className="logo nav-link">
+                  <img src= './logo512.png' alt='logo' className='logo' />    
+                </a>
+              </li>
+                <li className="nav-menu-item company-name">
+                  <a href="#" className="nav-menu-link nav-link ">QB</a>
+              </li>
+            </ul>
 
-        <ul className="nav-menu">
-          <li className="nav-menu-item">
-            <a href="#" className="nav-menu-link nav-link">Blog</a>
-          </li>
-          <li className="nav-menu-item">
-            <a href="#" className="nav-menu-link nav-link">Videos</a>
-          </li>
-          <li className="nav-menu-item">
-            <a href="#" className="nav-menu-link nav-link">Sobre mí</a>
-          </li>
-          <li className="nav-menu-item">
-            <a href="#" className="nav-menu-link nav-link nav-menu-link_active"
-              >Contacto</a
-            >
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </>
-
-    // <div classNameName="App">
-    //   <Navbar/>
-    //   <header classNameName="App-header">
-    //     <img src={logo} classNameName="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       classNameName="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+            <ul className="nav-menu">
+              {
+                menuItems.map(item =>(         
+                  <NavbarItem name={item.name} key={item.id} isActive={item.isActive}/>
+                ))
+              }              
+            </ul>
+          </nav>
+        </header>
+    </>
   );
 }
 
--- src/components/Items.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Items = ({name}) => {
-  return (
-    
-    <div className='navbar-item'>{name}</div>
-  )
-}
