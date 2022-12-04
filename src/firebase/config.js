3b5c017b3cc7c82a930f1353ee90065004f2f31c
ddessilvestri <silvestri.des@gmail.com>
2022-12-04 12:41:34 -0300

    firebase config using .env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,23 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBJCWAUFkGLFMtPcWyPOLoAPeD8YW26CCo",
-  authDomain: "ecommerce-latam-7f9f5.firebaseapp.com",
-  projectId: "ecommerce-latam-7f9f5",
-  storageBucket: "ecommerce-latam-7f9f5.appspot.com",
-  messagingSenderId: "90129208032",
-  appId: "1:90129208032:web:1e0992a900dd72e52cf685"
+  apiKey:             REACT_APP_API_KEY,
+  authDomain:         REACT_APP_AUTH_DOMAIN,
+  projectId:          REACT_APP_PROJECT_ID,
+  storageBucket:      REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  REACT_APP_MESSAGING_SENDER_ID,
+  appId:               REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
